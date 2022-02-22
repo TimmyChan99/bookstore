acc08c04e7c204033efea94456fb3aa51572a63d
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-22 13:04:25 +0100

    Add book and form components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Form from './components/Form';
+import Book from './components/Book';
 
 function App() {
   return (
     <div className="App">
-      
+      <Book />
+      <Form />
     </div>
   );
 }
