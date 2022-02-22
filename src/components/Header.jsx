d8ec8c3533fbd4a442e2a98acdd32cbd00dfa948
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-22 13:18:59 +0100

    Fix linters

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    <header>
+        <p>BookStore CMS</p>
+        <nav>
+            <ul>
+                <li>BOOKS</li>
+                <li>CATEGORIES</li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Header;
