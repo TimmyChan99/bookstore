0813db3ea9880e3aad6775bac37cdf36d1b3a600
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-23 12:00:56 +0100

    Get values from the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => (
-  <>
-    <h2>ADD NEW BOOK</h2>
-    <form>
-      <input type="text" />
-      <select name="categorie" id="categorie">
-        <option value="Action">Action</option>
-        <option value="Science Fiction">Science Fiction</option>
-        <option value="Economy">Economy</option>
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </>
-);
+
+const Form = () => {
+  const bookInfo = {
+    title: '',
+    category: '',
+  };
+  const [book, setBook] = useState(bookInfo);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setBook(book);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setBook((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="title" type="text" value={book.title} onChange={handleChange} />
+        <select name="category" id="categories" onChange={handleChange}>
+          <option value="none">Category</option>
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </>
+  );
+};
 export default Form;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,32 +3,31 @@ const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
-export const addBook = payload => ({
-    type: ADD_BOOK,
-    payload
-})
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
 
-export const removeBook = id => ({
-    type: REMOVE_BOOK,
-    payload: id
-})
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
 
-const reducer =  (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_BOOK:
-            
-            return [ ...state, 
-            { ...action.payload }
-            ];
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
 
-        case REMOVE_BOOK:
-            
-            return state.filter(book => book.id !== action.payload);
-        
-    
-        default:
-            return state;
-        }
-    };
-    
-    export default reducer;
+      return [...state,
+        { ...action.payload },
+      ];
+
+    case REMOVE_BOOK:
+
+      return state.filter((book) => book.id !== action.payload);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
