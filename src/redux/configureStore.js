a05d8023ff1727f7b548e9eade68fefc393dcfc5
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-23 19:16:47 +0100

    Remove unused code

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,14 +4,11 @@ import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
-  // additional reducers could be added here
 });
 
-/* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
   applyMiddleware(logger),
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-/* eslint-enable */
+
 export default store;
