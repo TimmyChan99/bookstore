76d2d9fefacf25f28cbd90d82d745562eee4dab9
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-23 15:34:29 +0100

    Add new book to booklist

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const Form = () => {
   const bookInfo = {
     title: '',
     category: '',
   };
+
+  const dispatch = useDispatch();
+
   const [book, setBook] = useState(bookInfo);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setBook(book);
-  };
+  const { title, category } = book;
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -21,18 +23,30 @@ const Form = () => {
     }));
   };
 
+  const submitBookToStore = (event) => {
+    event.preventDefault();
+    const newBook = {
+      id: uuidv4(), // make sure it's unique
+      title,
+      category,
+    };
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+  };
+
   return (
     <>
       <h2>ADD NEW BOOK</h2>
-      <form onSubmit={handleSubmit}>
-        <input name="title" type="text" value={book.title} onChange={handleChange} />
+      <form onSubmit={submitBookToStore}>
+        <input name="title" type="text" value={title} onChange={handleChange} />
         <select name="category" id="categories" onChange={handleChange}>
           <option value="none">Category</option>
           <option value="Action">Action</option>
           <option value="Science Fiction">Science Fiction</option>
           <option value="Economy">Economy</option>
         </select>
-        <button type="submit">ADD BOOK</button>
+        <button type="submit" onClick={submitBookToStore}>ADD BOOK</button>
       </form>
     </>
   );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -7,9 +7,11 @@ const reducer = combineReducers({
   // additional reducers could be added here
 });
 
+/* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
   applyMiddleware(logger),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
+/* eslint-enable */
 export default store;
