4fe527ca6174be49bd298d0ca8df85ec07ec2fe8
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-24 23:24:37 +0100

    Add style to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/header.css';
 
 const Header = () => (
   <header>
-    <p>BookStore CMS</p>
+    <span>BookStore CMS</span>
     <nav>
       <ul>
         <li>
-          <NavLink to="/">BOOKS</NavLink>
+          <NavLink className="nav_link" to="/">BOOKS</NavLink>
         </li>
         <li>
-          <NavLink to="/categorie">CATEGORIES</NavLink>
+          <NavLink className="nav_link" to="/categorie">CATEGORIES</NavLink>
         </li>
       </ul>
     </nav>
